8b97aa80ca6caeafd79d63f7062e6cd4bcbcc1c5
Guilherme Mendes <guilhermerm@s02bk02.sulamerica.br>
2021-03-16 17:05:27 -0300

    Project

---
+++ data-structure-stack-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace data_structure_stack_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ data-structure-stack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace data_structure_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
